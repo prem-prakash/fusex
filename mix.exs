9302eaa4be16b561968c10cfd9bc5535b045b300
Prem Prakash <prakash2ji@gmail.com>
2021-10-15 13:10:27 -0300

    v0.1.0

--- mix.exs
+++ mix.exs
@@ -1,27 +1,51 @@
 defmodule Fusex.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/prem-prakash/fusex"
+
   def project do
     [
       app: :fusex,
-      version: "0.1.0",
-      elixir: "~> 1.11",
+      name: "Fusex",
+      version: @version,
+      package: package(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+      elixir: "~> 1.10",
+      source_url: @source_url,
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      docs: [source_ref: "v#{@version}", main: "readme", extras: ["README.md"]],
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :fuse]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   defp deps do
     [
       {:fuse, "~> 2.5"}
+    ]
+  end
+
+  defp description do
+    """
+    A simple wrapper around erlang fuse library
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Prem Prakash"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
